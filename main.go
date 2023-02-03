a8b671a8eed1cd8b66d39cb9403f08f4dd103740
Oleksii <oleksii.ablitsov@gmail.com>
2023-02-03 17:48:37 +0200

    add response helpers

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/partizanen69/go-http-server/internal/database"
 )
 
 func main() {
 	serveMux := http.NewServeMux()
 	serveMux.HandleFunc("/", testHandler)
+	serveMux.HandleFunc("/err", testErrHandler)
 	
 	const addr = "localhost:8080"
 	srv := http.Server{
@@ -25,7 +30,45 @@ func main() {
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write([]byte("{}"))
+	respondWithJSON(w, 200, database.User{
+		Email: "some.email@gmail.com",
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "	application/json")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	
+	if payload == nil {
+		w.WriteHeader(code)
+		return
+	}
+
+	payloadJson, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(500)
+		errJsonBytes, _ := json.Marshal(errorBody{
+			Error: "Could not marshal resulting payload",
+		})
+		w.Write(errJsonBytes)
+		return
+	}
+
+	w.WriteHeader(code)
+	w.Write(payloadJson)
+}
+
+type errorBody struct {
+	Error string `json:"error"`
+}
+
+func respondWithError(w http.ResponseWriter, code int, err error) {
+	errBody := errorBody{
+		Error: err.Error(),
+	}
+	respondWithJSON(w, code, errBody)
+}
+
+func testErrHandler(w http.ResponseWriter, r *http.Request) {
+	respondWithError(w, 500, errors.New("this is a test error"))
 }
